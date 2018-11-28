2dc70f87a2277869454bec6ad0ac1e00c1aaa1e0
Sulthanm2d <45209800+Sulthanm2d@users.noreply.github.com>
2018-11-29 00:44:03 +0700

    Create page.h

---
+++ page.h
@@ -0,0 +1,21 @@
+#ifndef PAGE_H
+#define PAGE_H
+#include <stdio.h>
+#include <stdlib.h>
+
+void FrontPage();
+// Tampilan awal pada game
+
+void StartGame();
+// Memulai game (membaca map)
+
+void LoadGame();
+// Memulai game yang telah tersave (membaca map)
+
+void BacaMap();
+//Menampilkan Map
+
+void Quit();
+// Keluar dari game
+
+#endif
